edeb59494f890daa10b8eec65aee60156a6f9833
lamdaiphong.nz@hotmail.com <lamdaiphong.nz@hotmail.com>
2024-11-29 20:13:28 +1300

    today

--- main.tf
+++ main.tf
@@ -5,18 +5,18 @@ terraform {
       version = "~>5.40.0"
     }
     helm = {
-      source  = "hashicorp/helm"
+      source = "hashicorp/helm"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~>2.27.0"
     }
     http = {
-      source = "hashicorp/http"
+      source  = "hashicorp/http"
       version = "~>3.4.2"
     }
     kustomization = {
-      source = "kbst/kustomization"
+      source  = "kbst/kustomization"
       version = "~>0.9.6"
     }
   }
